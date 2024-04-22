443b75000dc7ab1f44d8ce850eb865a9d4132cd1
Felix <felixneumayr@gmx.de>
2024-04-22 10:51:52 +0200

    added CharacterCard Function

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,1 +1,29 @@
-export function CharacterCard() {}
+export function CharacterCard(src, name, occurence, status, type) {
+
+    const occurenceSum = occurence.length
+    const card = document.createElement("li");
+    card.innerHTML = `
+    <li class="card">
+          <div class="card__image-container">
+            <img
+              class="card__image"
+              src="${src}"
+              alt="${name}"
+            />
+            <div class="card__image-gradient"></div>
+          </div>
+          <div class="card__content">
+            <h2 class="card__title">${name}</h2>
+            <dl class="card__info">
+              <dt class="card__info-title">Status</dt>
+              <dd class="card__info-description">${status}</dd>
+              <dt class="card__info-title">Type</dt>
+              <dd class="card__info-description">${type}</dd>
+              <dt class="card__info-title">Occurrences</dt>
+              <dd class="card__info-description">${occurenceSum}</dd>
+            </dl>
+          </div>
+        </li>
+    `
+
+}
